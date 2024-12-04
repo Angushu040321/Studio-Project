72cf11f121ae66ed0874405cc7d3f89077ca732a
Angus Hu <jentinghu0321@gmail.com>
2024-12-04 14:27:43 -0500

    sword

---
+++ script-test.cs
@@ -0,0 +1,20 @@
+public enum Mantras{
+    Flamecharm , 
+    Thundercall,
+    Frostdraw,
+    Galebreathe,
+    Shadowcast,
+    Ironsing,
+    Bloodrend
+}
+public class Sword{
+    private Mantras mantrasType_;
+    private double damage_;
+
+    
+
+}   
+
+void main(){
+
+}
